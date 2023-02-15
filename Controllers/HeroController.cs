71a9e0ebd3a855c185926305ce7405b3c04efe71
norden90 <nordfeldt.andreas@gmail.com>
2023-02-15 14:49:31 +0100

    First

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPi.Models;
+
+namespace SuperHeroAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        private static List<Hero> _heroes = new List<Hero>()
+        {
+            new Hero() { Id = 1, Name = "Tony Lark", Team = "Avengers", SuperHeroName = "Ironman" },
+            new Hero() { Id = 2, Name = "Bruce Wayne", Team = "Justice League", SuperHeroName = "Batman" }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_heroes);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_heroes.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Hero hero)
+        {
+            _heroes.Add(hero);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Hero hero)
+        {
+            var heroToUpdate = _heroes.FirstOrDefault(x => x.Id == hero.Id);
+            if (heroToUpdate == null)
+            {
+                return NotFound();
+            }
+            heroToUpdate.Name = hero.Name;
+            heroToUpdate.Team = hero.Team;
+            heroToUpdate.SuperHeroName = hero.SuperHeroName;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var heroToDelete = _heroes.FirstOrDefault(x => x.Id == id);
+            if (heroToDelete == null)
+            {
+                return NotFound();
+            }
+            _heroes.Remove(heroToDelete);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Hero.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SuperHeroAPi.Models;
+
+public class Hero
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SuperHeroName { get; set; }
+    public string Team{ get; set; }
+}
